a99947a4264ab35794adc075857627c64cc0935a
Samuel-Henrique12 <sdikdplayd@gmail.com>
2025-08-06 17:59:10 -0300

    Pre-Super-Gestão

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get('/', function () {
+    return 'Olá, seja bem vindo ao curso';
+}); */
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}',
+    function(string $nome = 'Julio',int $categoria = 1) {
+    echo "Estamos aqui: $nome - $categoria";
+})-> where('categoria_id', '[0-9]+')
+    ->where('nome', '[a-zA-Z]+');
