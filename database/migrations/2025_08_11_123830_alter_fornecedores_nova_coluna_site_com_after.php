5c7dfcb61a2484c2641227feb172a4d79239c340
Samuel-Henrique12 <sdikdplayd@gmail.com>
2025-08-11 18:00:15 -0300

    DB Teste W/Factory (Faker) MK1

--- database/migrations/2025_08_11_123830_alter_fornecedores_nova_coluna_site_com_after.php
+++ database/migrations/2025_08_11_123830_alter_fornecedores_nova_coluna_site_com_after.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 50);
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('fornecedores', function(Blueprint $table) {
+           $table->string('site', 150)->after('nome')->nullable();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function(Blueprint $table) {
+        $table->dropColumn('site');
+        });
     }
 };
---
+++ database/seeders/SiteContatoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /*$contato = new siteContato();
+        $contato->nome = 'Sistema SG';
+        $contato->telefone = '(11) 99999-9999';
+        $contato->email = 'contato@sg.com.br';
+        $contato->motivo_contato = 1;
+        $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
+        $contato->save();*/
+        \App\Models\SiteContato::factory()->count(100)->create();
+    }
+}
