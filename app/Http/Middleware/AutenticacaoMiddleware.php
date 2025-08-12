4206a8f29a5c50b7578773657fc5d0c588bd7996
Samuel-Henrique12 <sdikdplayd@gmail.com>
2025-08-12 17:51:28 -0300

    Middlewares MK1

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -5,10 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\LogAcesso;
 
-
-class LogAcessoMiddleware
+class AutenticacaoMiddleware
 {
     /**
      * Handle an incoming request.
@@ -17,9 +15,7 @@ class LogAcessoMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $ip = $request->server->get('REMOTE_ADDR');
-        $rota = $request->getRequestUri();
-        LogAcesso::create(['log' => "IP $ip requisitou a rota $rota"]);
-        return Response('Chegamos no middleware e finalizamos no próprio middleware.');
+        //return $next($request);
+        return Response('acesso negado! Rota exige autenticação!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,14 @@ use App\Http\Controllers\ProdutosController;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato.salvar');
 Route::get('/login', function() {return "Login";})->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', [ClientesController::class, 'clientes'])->name('app.clientes');
+    Route::middleware('autenticacao')->get('/clientes', [ClientesController::class, 'clientes'])->name('app.clientes');
     Route::get('/fornecedores', [FornecedorController::class, 'fornecedores'])->name('app.fornecedores');
     Route::get('/produtos', [ProdutosController::class, 'produtos'])->name('app.produtos');
 });
